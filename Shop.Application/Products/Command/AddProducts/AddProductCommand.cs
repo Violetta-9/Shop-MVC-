48b2765850a38872552592c39d2921bf9292357d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-09-18 12:20:27 +0300

    1.Добавила ProductsController 2.Добавила во View Create 3. Добавила в Shop.Application AddProductCommand,AddProductCommandHandler

---
+++ Shop.Application/Products/Command/AddProducts/AddProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Shop.Application.Products.Command.AddProducts
+{
+   public class AddProductCommand : IRequest<int>
+   {  
+       public string Name { get; set; }
+       public string Description { get; set; }
+       public double Price { get; private set; }
+       public string Image { get; private set; }
+    }
+}
---
+++ Shop.Application/Products/Command/AddProducts/AddProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Shop.Data;
+using Shop.Domain.Models;
+
+namespace Shop.Application.Products.Command.AddProducts
+{
+    class AddProductCommandHandler : IRequestHandler<AddProductCommand, int>
+    {
+        private readonly ApplicationDbContext _db;
+        public AddProductCommandHandler(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<int> Handle(AddProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product(request.Name, request.Description, request.Price, request.Image);
+            _db.Products.Add(product);
+            await _db.SaveChangesAsync(cancellationToken);
+            return product.Id;
+
+        }
+    }
+}
