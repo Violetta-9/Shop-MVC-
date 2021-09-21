7f0e365623b5917abaaf8d958a1d0ca0952e419b
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-09-21 11:42:56 +0300

    1.Добавила контроллер для продуктов к Админу 2. Добавила представление Index для продуктов в Админ 3.Добавила в Админ _viewImports,_viewStart

--- Shop/Areas/Admin/Controllers/ProductsController.cs
+++ Shop/Areas/Admin/Controllers/ProductsController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Shop.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class ProductsController : Controller
     {
         // GET: ProductsController
