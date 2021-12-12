32fd53c33ceda0a19e0fe66a4b271c733de0efe0
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2021-12-12 23:53:48 +0300

    1.подсчет активных звезд

--- Shop/wwwroot/js/Rating.js
+++ Shop/wwwroot/js/Rating.js
@@ -1,5 +1,6 @@
 ﻿var rating = document.querySelector('.rating');
 var ratingItem = document.querySelectorAll('.rating-item');
+
 rating.onclick = function (e) {
     var target = e.target;
     if (target.classList.contains('rating-item')) {
@@ -10,9 +11,12 @@ rating.onclick = function (e) {
 rating.onmouseover = function (e) {
     var target = e.target;
     if (target.classList.contains('rating-item')) {
+        
         removeClass(ratingItem, 'current-active');
+       
         target.classList.add('active', 'current-active');
         mouseOverActiveClass(ratingItem);
+       
     }
 }
 rating.onmouseout = function () {
@@ -22,38 +26,55 @@ rating.onmouseout = function () {
 function removeClass(arr) {
     for (var i = 0, iLen = arr.length; i < iLen; i++) {
         for (var j = 1; j < arguments.length; j++) {
-            ratingItem[i].classList.remove(arguments[j])
+            ratingItem[i].classList.remove(arguments[j]);
         }
+      
     }
 }
 function addClass(arr) {
+ 
     for (var i = 0, iLen = arr.length; i < iLen; i++) {
         for (var j = 1; j < arguments.length; j++) {
-            ratingItem[i].classList.add(arguments[j])
+            ratingItem[i].classList.add(arguments[j]);
+           
         }
     }
+    
+   
 }
 function mouseOverActiveClass(arr) {
+
     for (var i = 0, iLen = arr.length; i < iLen; i++) {
         if (arr[i].classList.contains('active')) {
             break;
         } else {
             arr[i].classList.add('active');
+            
         }
+       
     }
+  
 
 }
 function mouseOutActiveClass(arr) {
+    var num = 0;
     for (var i = arr.length - 1; i >= 0; i--) {
         if (arr[i].classList.contains('current-active')) {
             break;
         } else {
             arr[i].classList.remove('active');
+            
         }
     }
+   
+    var f = document.getElementsByClassName("rating-item");
+    for (var i = 0; i < 5; i++) {
+        if (f[i].classList.contains("active")) {
+            num++;
+        }
+    }
+    var rf = document.getElementById("ratingValue");
+    rf.value = num;
+    console.log(rf);
 
 }
- function ratingValue() {
-     var result = document.querySelector("rating-item");
-     console.log(result);
- }
